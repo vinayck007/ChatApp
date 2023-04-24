b0afbad75ca6724abd4a989c1fa5176d3ac9653a
vinayck007 <vinayck007@gmail.com>
2023-04-24 11:56:02 +0530

    encrypting password

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,18 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 exports.signup = async (req, res) => {
   const { name, email, phone, password } = req.body;
   
   try {
-    const user = await User.findOne({ where: { email } });
-    if (user) {
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
-    await User.create({ name, email, phone, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({ name, email, phone, password: hashedPassword });
     res.status(201).json({ user });
   } catch (error) {
     console.error(error);
